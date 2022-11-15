432730b9753fafbb496089eaf39357a8a8635b98
Tommy Tran <haitran1995@gmail.com>
2022-11-15 03:18:56 -0500

    create the delete query for todos

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(express.json()); //req.body
 // create a todo
 // our req.body will be our description which will be our VALUE($1)
 // res.json returns first Obj in Array which is the id and description from db
+// "Insert into todo table, under column description the req.body and return all"
 app.post("/todos", async (req, res) => {
   try {
     const { description } = req.body;
@@ -30,6 +31,7 @@ app.post("/todos", async (req, res) => {
 });
 
 // get all todo
+// "Select all rows from todo table"
 app.get("/todos", async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todo");
@@ -42,6 +44,7 @@ app.get("/todos", async (req, res) => {
 // get a specific todo
 // id will be our req.params and will be our id for the query
 // json returns id and description of specific todo from db
+// "Select all rows from todo table, Where todo_id equals req.params"
 app.get("/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -57,6 +60,7 @@ app.get("/todos/:id", async (req, res) => {
 
 // update a todo
 // uses both req.params and req.body to find new description and the id of current description
+// "Update todo table, the description to req.body, WHERE todo_id equals req.params"
 app.put("/todos/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -66,13 +70,24 @@ app.put("/todos/:id", async (req, res) => {
       [description, id]
     );
 
-    res.json("Todo was updated!")
+    res.json("Todo was updated!");
   } catch (error) {
     console.error(error.message);
   }
 });
 
 // delete a todo
+// "Delete from todo table where todo_id equals the req.params"
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+    res.json("Todo was deleted!");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
