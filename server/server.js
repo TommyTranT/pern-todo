46e629bf45c9a0bf43382700010ee22f558e2b8a
Tommy Tran <haitran1995@gmail.com>
2022-11-15 01:47:20 -0500

    create and connect to database

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const cors = require("cors");
 const PORT = 8080;
 
+// Connect to database
+const pool = require("./db");
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
