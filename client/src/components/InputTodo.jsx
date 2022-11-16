b8f050c2d3ab192c827d2f1d9e45db476b047605
Tommy Tran <haitran1995@gmail.com>
2022-11-16 17:34:33 -0500

    add data to our db through fetch post form

--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -4,10 +4,26 @@ import { useState } from "react";
 const InputTodo = () => {
   const [description, setDescription] = useState("");
 
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:8080/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      console.log(response)
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Pern Todo List</h1>
-      <form className="d-flex mt-5">
+      <form className="d-flex mt-5" onSubmit={onSubmitForm }>
         <input
           type="text"
           className="form-control"
