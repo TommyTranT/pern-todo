b508f161bb8400e37a0d11bffdc4ae3652a9f67b
Tommy Tran <haitran1995@gmail.com>
2022-11-16 17:08:54 -0500

    add description state and style form and button

--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -1,12 +1,20 @@
 import React, { Fragment } from "react";
+import { useState } from "react";
 
 const InputTodo = () => {
+  const [description, setDescription] = useState("");
+
   return (
     <Fragment>
       <h1 className="text-center mt-5">Pern Todo List</h1>
-      <form>
-        <input type="text" />
-        <button>Add </button>
+      <form className="d-flex mt-5">
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button className="btn btn-success">Add </button>
       </form>
     </Fragment>
   );
