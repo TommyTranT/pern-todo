5fc20b3a3cd846b386f989e2da34ec6fb4930c01
Tommy Tran <haitran1995@gmail.com>
2022-11-17 18:59:37 -0500

    add table to listtodos

--- client/src/components/ListTodos.jsx
+++ client/src/components/ListTodos.jsx
@@ -1,9 +1,36 @@
-
+import { Fragment } from "react";
 
 const ListTodos = () => {
   return (
-    <h1 className="Hi">List Todos</h1>
-  )
+    <Fragment>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>Doe</td>
+          </tr>
+          <tr>
+            <td>July</td>
+            <td>July</td>
+            <td>July</td>
+          </tr>
+          <tr>
+            <td>July</td>
+            <td>July</td>
+            <td>July</td>
+          </tr>
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default ListTodos;
